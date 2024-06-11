b8ff46e13012ee307be5805743c855b343629ba7
Logan McAnsh <logan@mcan.sh>
2024-06-11 16:03:27 -0400

    chore: lift session to loadContext and add session secret

--- app/routes/_._index.tsx
+++ app/routes/_._index.tsx
@@ -6,7 +6,6 @@ import {
 import { useLoaderData } from "@remix-run/react";
 import { Feed } from "~/components/feed";
 import { api } from "~/.server/api";
-import { sessionStorage } from "~/.server/session";
 import { z } from "zod";
 
 export const meta: MetaFunction = () => {
@@ -16,65 +15,69 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-export const loader = unstable_defineLoader(async ({ request, response }) => {
-  const cookie = request.headers.get("Cookie");
-  const session = await sessionStorage.getSession(cookie);
-  const url = new URL(request.url);
+export const loader = unstable_defineLoader(
+  async ({ context, request, response }) => {
+    const cookie = request.headers.get("Cookie");
+    const session = await context.sessionStorage.getSession(cookie);
+    const url = new URL(request.url);
 
-  const pageParam = url.searchParams.get("page");
+    const pageParam = url.searchParams.get("page");
 
-  if (pageParam && pageParam === "1") {
-    response.status = 302;
-    response.headers.set("Location", "/");
-    throw response;
-  }
-
-  const page = Number(pageParam);
-
-  const hidden = session.get("hidden") || [];
-  const { has_more, stories } = await api.get_posts(
-    "/topstories.json",
-    hidden,
-    page || 1,
-  );
-  return { stories, page, has_more };
-});
-
-export const action = unstable_defineAction(async ({ request, response }) => {
-  const cookie = request.headers.get("Cookie");
-  const session = await sessionStorage.getSession(cookie);
-  const formData = await request.formData();
-  const intent = formData.get("intent");
-
-  const schema = z.object({
-    intent: z.string(),
-    id: z.coerce.number(),
-  });
-
-  const result = schema.parse(Object.fromEntries(formData.entries()));
-
-  switch (intent) {
-    case "hide": {
-      const hidden = session.get("hidden") || [];
-      const hiddenSet = new Set(hidden);
-      hiddenSet.add(result.id);
-      session.set("hidden", Array.from(hiddenSet));
-
+    if (pageParam && pageParam === "1") {
       response.status = 302;
-      response.headers.append(
-        "Set-Cookie",
-        await sessionStorage.commitSession(session),
-      );
       response.headers.set("Location", "/");
       throw response;
     }
-    default: {
-      console.error("Invalid intent", intent);
-      response.status = 422;
-      throw response;
+
+    const page = Number(pageParam);
+
+    const hidden = session.get("hidden") || [];
+    const { has_more, stories } = await api.get_posts(
+      "/topstories.json",
+      hidden,
+      page || 1,
+    );
+    return { stories, page, has_more };
+  },
+);
+
+export const action = unstable_defineAction(
+  async ({ context, request, response }) => {
+    const cookie = request.headers.get("Cookie");
+    const session = await context.sessionStorage.getSession(cookie);
+    const formData = await request.formData();
+    const intent = formData.get("intent");
+
+    const schema = z.object({
+      intent: z.string(),
+      id: z.coerce.number(),
+    });
+
+    const result = schema.parse(Object.fromEntries(formData.entries()));
+
+    switch (intent) {
+      case "hide": {
+        const hidden = session.get("hidden") || [];
+        const hiddenSet = new Set(hidden);
+        hiddenSet.add(result.id);
+        session.set("hidden", Array.from(hiddenSet));
+
+        response.status = 302;
+        response.headers.append(
+          "Set-Cookie",
+          await context.sessionStorage.commitSession(session),
+        );
+        response.headers.set("Location", "/");
+        throw response;
+      }
+      default: {
+        console.error("Invalid intent", intent);
+        response.status = 422;
+        throw response;
+      }
     }
-  }
-});
+  },
+);
 
 export default function IndexPage() {
   const data = useLoaderData<typeof loader>();
--- load-context.ts
+++ load-context.ts
@@ -1,4 +1,11 @@
+import type { AppLoadContext } from "@remix-run/cloudflare";
 import type { PlatformProxy } from "wrangler";
+import { createCookieSessionStorage } from "@remix-run/cloudflare";
+import {
+  createTypedSessionStorage,
+  type TypedSessionStorage,
+} from "remix-utils/typed-session";
+import { z } from "zod";
 
 // When using `wrangler.toml` to configure bindings,
 // `wrangler types` will generate types for those bindings
@@ -10,8 +17,49 @@ interface Env {}
 
 type Cloudflare = Omit<PlatformProxy<Env>, "dispose">;
 
+const sessionSchema = z.object({
+  hidden: z.array(z.number()).optional(),
+});
+
 declare module "@remix-run/cloudflare" {
   interface AppLoadContext {
     cloudflare: Cloudflare;
+    sessionStorage: TypedSessionStorage<typeof sessionSchema>;
   }
 }
+
+type GetLoadContext = (args: {
+  request: Request;
+  context: { cloudflare: Cloudflare }; // load context _before_ augmentiation
+}) => AppLoadContext;
+
+// shared implementation compatible with Vite, Wrangler, and Cloudflare Pages
+export const getLoadContext: GetLoadContext = ({ context }) => {
+  const envSchema = z.object({
+    SESSION_SECRET: z.string(),
+  });
+
+  const env = envSchema.parse(context.cloudflare.env);
+
+  const sessionStorage = createCookieSessionStorage({
+    cookie: {
+      name: "__session",
+      httpOnly: true,
+      sameSite: "lax",
+      path: "/",
+      maxAge: 60 * 60 * 24 * 7, // 1 week
+      secure: process.env.NODE_ENV === "production",
+      secrets: [env.SESSION_SECRET],
+    },
+  });
+
+  const typedSessionStorage = createTypedSessionStorage({
+    sessionStorage,
+    schema: sessionSchema,
+  });
+
+  return {
+    ...context,
+    sessionStorage: typedSessionStorage,
+  };
+};
